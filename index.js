e79bc9aa3e5f2c140124a230535eefcaf1172919
Dominik Mast <dominik-mast@web.de>
2017-04-05 18:06:33 +0000

    program complete

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 var express = require("express");
 var app = express();
 var Bing = require('node-bing-api')({ accKey: "1fc3f4c636b44449be7232e39f24e679" });
+const mongoClient = require("mongodb").MongoClient;
+const dburl = "mongodb://shortener:shortener@ds145220.mlab.com:45220/shortener"//"mongodb://localhost:27017/shortener"
 
 
 
 
-
-app.get("/:eingabe", function(req, res){
+app.get("/:eingabe*", function(req, res){
     var eingabe = req.params.eingabe;
     var set= req.query.offset;
-    
-    
-    var set = set || 5; 
+    var set = set || 1; 
  
-Bing.images(eingabe, {
-    top: set,  
-  }, function (err,reso,data){
-    res.send(data.value.map(show));
+    if (req.params[0].length>0 && req.params.eingabe=="latest"&& req.params[0]=="/imagesearch"){
+        
+        mongoClient.connect(dburl, function(err,db){
+            db.collection("imagesearch").find({},{_id:0}).toArray(function(err,result){
+                var sort = result.sort().reverse();
+                res.send(sort);
+            })
+        })
+            
+    }else{
+        Bing.images(eingabe, {
+            top: set,  
+            }, function (err,reso,data){
+            console.log(data.thumbnaiUrl)
+            res.send(data.value.map(show));
     
-    
-  });
+        });
 
+    
+    mongoClient.connect(dburl, function(err,db){
+       
+        db.collection("imagesearch").insert({
+            search: eingabe,
+            when: new Date()
+        })
+        
+        db.close();
+    })
+    
+
+
+}
 })
 
 app.listen((process.env.PORT||8080|| 5000), function(){
@@ -34,7 +57,7 @@ function show (data){
     return ({
         name: data.name,
         url: data.contentUrl,
-        thumbnailUrl: data.thumbnaiUrl,
+        thumbnailUrl: data.thumbnailUrl,
         
     })
     
