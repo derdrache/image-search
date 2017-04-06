ceed1bcee9273f747e6dfd44a42c76054a5aa2bf
Dominik Mast <dominik-mast@web.de>
2017-04-06 07:13:40 +0000

    little change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dburl = "mongodb://shortener:shortener@ds145220.mlab.com:45220/shortener"/
 app.get("/:eingabe*", function(req, res){
     var eingabe = req.params.eingabe;
     var set= req.query.offset;
-    var set = set || 1; 
+    var set = set || 10; 
  
     if (req.params[0].length>0 && req.params.eingabe=="latest"&& req.params[0]=="/imagesearch"){
         
